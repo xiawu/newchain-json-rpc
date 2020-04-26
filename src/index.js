638ee30275425d06b6974d7584ccb22c3a2738cf
weixuefeng <weixuefeng1018@gmail.com>
2020-04-26 23:42:19 +0800

    fix: request id can not be string

--- src/index.js
+++ src/index.js
@@ -140,6 +140,9 @@ function fetchConfigFromReq({ network, req, source }) {
 
 // strips out extra keys that could be rejected by strict nodes like parity
 function normalizeReq(req) {
+  if(typeof(req.id) == 'string') {
+    req.id = 1
+  }
   return {
     id: req.id,
     jsonrpc: req.jsonrpc,
